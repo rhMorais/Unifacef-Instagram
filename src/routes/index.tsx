0df0406144b463683f8397e7e90e7b29dbf8aa0b
Rafael Morais <rafael.morais@smn.com.br>
2020-05-30 09:03:57 -0300

    iniciando projeto

---
+++ src/routes/index.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+
+import Home from '../containers/home';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {StatusBar} from 'react-native';
+
+const Stack = createStackNavigator();
+
+export enum ROUTES_NAMES {
+  Home = 'Home',
+}
+
+function Routes() {
+  return (
+    <NavigationContainer>
+      <StatusBar backgroundColor="black" barStyle="dark-content" />
+      <Stack.Navigator initialRouteName={ROUTES_NAMES.Home}>
+        <Stack.Screen
+          options={{
+            headerTitleAlign: 'center',
+            headerTitle: 'Unifacef Gram',
+            headerTintColor: 'white',
+            headerStyle: {
+              backgroundColor: 'black',
+            },
+          }}
+          name={ROUTES_NAMES.Home}
+          component={Home}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Routes;
